9553adacb6bfa5aef38dc24a30b3e9cc9c1739b0
Yuri <yuribattaglin@hotmail.com>
2021-12-01 17:07:51 -0300

    O que faltou

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,25 @@
+export default {
+    colors: {
+        primary: '#21223A',
+        secondary: '#18DF9A',
+        secondary_light: 'rgba(24,223,154, 0.3)',
+
+        success: '#12A454',
+        success_light: 'rgba(18,164,84, 0.5)',
+
+        attention: '#E83F5B',
+        attention_light: 'rgba(232, 63, 91, 0.5)',
+
+        shape: '#FFFFFF',
+        title: '#363F5F',
+        text: '#969cb2',
+        text_dark: '#000000',
+        background: '#f0f2f5',
+    },
+
+    fonts: {
+        regular: 'Poppins_400Regular',
+        medium: 'Poppins_500Medium',
+        bold: 'Poppins_700Bold',
+    }
+}
