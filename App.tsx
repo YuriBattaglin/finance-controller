e409513c685ab90d89a036ad66dcadae4f1ba313
Yuri <yuribattaglin@hotmail.com>
2021-12-01 16:56:10 -0300

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import AppLoading from 'expo-app-loading';
+import { ThemeProvider } from 'styled-components';
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold
+} from '@expo-google-fonts/poppins'
+
+import { NavigationContainer } from '@react-navigation/native'
+import {AppRoutes} from './src/routes/app.routes'
+
+import theme from './src/global/styles/theme';
+import { StatusBar } from 'react-native';
+
+export default function App(){
+  const [ fontsLoaded ] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  });
+
+  if(!fontsLoaded){
+    return <AppLoading />
+  }
+
+  return(
+    <ThemeProvider theme={theme}>
+      <NavigationContainer>
+        <StatusBar barStyle={'light-content'} />
+        <AppRoutes />
+      </NavigationContainer>
+    </ThemeProvider>
+  ) 
+}
+
