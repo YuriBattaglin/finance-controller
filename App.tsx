35a99e0a8bd1c78a9c023354fef2001d13b60aac
Yuri <yuribattaglin@hotmail.com>
2021-12-02 12:29:22 -0300

    Atualização geral projeto

---
+++ App.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import AppLoading from 'expo-app-loading';
+import { ThemeProvider } from 'styled-components';
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold
+} from '@expo-google-fonts/poppins'
+
+import { NavigationContainer } from '@react-navigation/native'
+import {AppRoutes} from './src/routes/app.routes'
+import { SignIn } from './src/screens/Signin';
+import { AuthProvider } from './src/hooks/auth';
+
+import theme from './src/global/styles/theme';
+import { StatusBar } from 'react-native';
+
+export default function App(){
+  const [ fontsLoaded ] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  });
+
+  if(!fontsLoaded){
+    return <AppLoading />
+  }
+
+  return(
+    <ThemeProvider theme={theme}>
+      <NavigationContainer>
+        <StatusBar barStyle={'light-content'} />
+
+        <AuthProvider>
+          <SignIn />
+        </AuthProvider>
+
+      </NavigationContainer>
+    </ThemeProvider>
+  ) 
+}
+
